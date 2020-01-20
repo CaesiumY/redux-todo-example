d30f33eee10f47563fedbf1de0ddd5e3c99c7166
caesiumy <mn0658@naver.com>
2020-01-20 14:24:09 +0900

    feat: Add redux store

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { addTodo } from "./actions";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
+
 import "./css/index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+const render = () =>
+  ReactDOM.render(
+    <div id="app">
+      <AddTodo
+        addTodo={text => {
+          store.dispatch(addTodo(text));
+        }}
+      />
+      <TodoList todos={store.getState()} />
+    </div>,
+
+    document.getElementById("root")
+  );
+
+render();
+store.subscribe(render);
