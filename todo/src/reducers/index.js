bca94c2e15501ccaff533157ffe503e22c488d04
caesiumy <mn0658@naver.com>
2020-01-24 23:23:22 +0900

    chore: Add comments to understand more easily

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -17,6 +17,8 @@ function todos(state = [], action) {
         Object.assign({}, state[action.index], {
           isComplete: !state[action.index].isComplete
         }),
+        // 인덱스 값의 앞과 뒤는 그대로 두고, 해당하는 인덱스를 가진 값의 데이터를 불러와서 새로 데이터를 만든다.
+        // Object.assign를 통해 {} 안에 해당 인덱스에 해당하는 state의 데이터를 일단 모두 집어넣고, isComplete를 추가해준다.
         ...state.slice(action.index + 1)
       ];
     case DELETE_TODO:
